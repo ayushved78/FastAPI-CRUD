547f28caedb842ff44fb17e9542cf05f0eb98ded
ayushved78 <ayush.ved78@gmail.com>
2023-06-20 00:21:30 +0530

    Books Requests

--- books_api/books_api.py
+++ books_api/books_api.py
@@ -1,13 +1,20 @@
 """Main module."""
 
 from fastapi import FastAPI,Body
-from data.Books_data import Books
+from data.new_book import New_Books
 
 app = FastAPI()
-BOOK = []
+
+@app.get("/")
+async def first_route():
+    return {"message":"Route to /book for more information!"}
 @app.get("/book")
 async def read_book():
-    return Books
+    return New_Books
+@app.post("/book/create_book_without_validation")
+async def create_book_without_validation(no_validation=Body()):
+    New_Books.append(no_validation) 
+    return {"message":"Book added to the record withut validation!"}
 
 class BookDataAPI:
     id: int
@@ -32,6 +39,18 @@ class BookDataAPI:
         self.year = year
         self.description = description
         self.rating = rating
+
+class BookRequest(BaseModel):
+    id: int
+    author: str
+    country: str
+    imageLink: str
+    language: str
+    pages: int
+    title: str
+    year: int
+    description: str
+    rating: float
 
 
 if __name__ == '__main__':
