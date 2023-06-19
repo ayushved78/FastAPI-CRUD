bdd2976549abde69ab110243e392550282943e96
ayushved78 <ayush.ved78@gmail.com>
2023-06-19 23:47:26 +0530

    Books Class

--- books_api/books_api.py
+++ books_api/books_api.py
@@ -1,1 +1,40 @@
 """Main module."""
+
+from fastapi import FastAPI,Body
+from data.Books_data import Books
+
+app = FastAPI()
+BOOK = []
+@app.get("/book")
+async def read_book():
+    return Books
+
+class BookDataAPI:
+    id: int
+    author: str
+    country: str
+    imageLink: str
+    language: str
+    pages: int
+    title: str
+    year: int
+    description: str
+    rating: float
+
+    def __init__(self, id, author, country, imageLink, language, pages, title, year, description, rating):
+        self.id = id
+        self.author = author
+        self.country = country
+        self.imageLink = imageLink
+        self.language = language
+        self.pages = pages
+        self.title = title
+        self.year = year
+        self.description = description
+        self.rating = rating
+
+
+if __name__ == '__main__':
+    print(123)
+
+
