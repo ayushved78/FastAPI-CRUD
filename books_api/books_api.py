a58ad1d789e747783c96a199b422f5246eef3b49
ayushved78 <ayush.ved78@gmail.com>
2023-06-21 08:40:45 +0530

    data Validation

--- books_api/books_api.py
+++ books_api/books_api.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI,Body
 from data.new_book import New_Books
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -42,16 +43,22 @@ class BookDataAPI:
 
 class BookRequest(BaseModel):
     id: int
-    author: str
+    author: str = Field(min_length=1)
     country: str
     imageLink: str
-    language: str
-    pages: int
-    title: str
+    language: str = Field(min_length=3)
+    pages: int = Field(gt=50,lt=1500) 
+    title: str = Field(min_length=3)
     year: int
-    description: str
-    rating: float
+    description: str = Field(min_length=1,max_length=100)
+    rating: float = Field(gt=-1,lt=6)
 
+@app.post("/book/create_book")
+async def create_book(book_request: BookRequest):
+    print(type(book_request))
+    # convert the BookDataAPI type to dcit before moving as a new book
+    new_book = BookDataAPI(**book_request.dict())
+    New_Books.append(new_book)
 
 if __name__ == '__main__':
     print(123)
